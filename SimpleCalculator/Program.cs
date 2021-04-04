4831f6d83c6610a77c11082d88c89b0d18bf44c4
Emil Hristov <emodavitkov@abv.bg>
2021-04-05 00:51:01 +0300

    OK

---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrInput = Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .Reverse()
+                .ToArray();
+           
+            Stack<string> stackRepository = new Stack<string>(arrInput);
+
+            while (stackRepository.Count>1)
+            {
+                int firstDigit = int.Parse(stackRepository.Pop());
+                string operand = stackRepository.Pop();
+                int secondDigit = int.Parse(stackRepository.Pop());
+                int thirdDigit = 0;
+                switch (operand)
+                {
+                    case "+":
+                        thirdDigit = firstDigit + secondDigit;
+                        break;
+
+                    case "-":
+                        thirdDigit = firstDigit - secondDigit;
+                        break;
+
+                    default:
+                        break;
+                }
+
+                stackRepository.Push(thirdDigit.ToString());
+            }
+
+            Console.WriteLine(stackRepository.Pop());
+        }
+    }
+}
--- StackSum/Program.cs
+++ StackSum/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace StackSum
 {
@@ -6,7 +8,60 @@ namespace StackSum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arrNumbers = Console.ReadLine()
+                 .Split()
+                 .Select(int.Parse)
+                 .ToArray();
+
+            Stack<int> stackNumbers = new Stack<int>(arrNumbers);
+
+            while (true)
+            {
+                string input = Console.ReadLine().ToLower();
+
+                if (input == "end")
+                {
+                    break;
+                }
+
+                string[] commands = input
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string command = commands[0];
+
+                switch (command)
+                {
+                    case "add":
+                        int numberOne = int.Parse(commands[1]);
+                        int numberTwo = int.Parse(commands[2]);
+                        stackNumbers.Push(numberOne);
+                        stackNumbers.Push(numberTwo);
+                        break;
+
+                    case "remove":
+                        int countRemove = int.Parse(commands[1]);
+
+                        if (countRemove>stackNumbers.Count)
+                        {
+                            continue;
+                        }
+
+                        for (int i = 0; i < countRemove; i++)
+                        {
+                            stackNumbers.Pop();
+                        }
+
+                        break;
+
+                    default:
+                        break;
+                }
+                
+
+            }
+            
+
+            Console.WriteLine($"Sum: {stackNumbers.Sum()}");
         }
     }
 }
