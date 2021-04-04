04925ca8de4f51ac4e1687bd8bef8196cc779095
Emil Hristov <emodavitkov@abv.bg>
2021-04-04 16:40:27 +0300

    GO

---
+++ Advance_2021_Stack_AND_Queues/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Advance_2021_STACKS_AND_QUEUES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<char> strinngReverse = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                strinngReverse.Push(input[i]);
+            }
+
+            foreach (var item in strinngReverse)
+            {
+                Console.Write(item);
+
+            }
+        }
+    }
+}
