69db0b1867e277e4d2c061c112a1162c49e4bd32
Emil Hristov <emodavitkov@abv.bg>
2021-06-23 01:33:47 +0300

    Go

---
+++ Bombs/Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Channels;
+
+namespace Bombs
+{
+    public class Program
+    {
+        static void Main()
+        {
+
+            Queue<int> bombEffects = new Queue<int>(Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList());
+
+            Stack<int> bombCasings = new Stack<int>(Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList());
+
+
+
+            int daturaBombs = 0;
+            int cherryBombs = 0;
+            int smokeDecoyBombs = 0;
+
+            while (bombEffects.Count > 0 && bombCasings.Count > 0)
+            {
+                int effect = bombEffects.Dequeue();
+                int casing = bombCasings.Pop();
+
+                int sum = effect + casing;
+
+                if (sum == 40)
+                {
+                    daturaBombs++;
+
+                    if (daturaBombs >= 3 && cherryBombs >= 3 && smokeDecoyBombs >= 3)
+                    {
+
+                        break;
+                    }
+
+                    continue;
+                }
+
+                else if (sum == 60)
+                {
+                    cherryBombs++;
+
+                    if (daturaBombs >= 3 && cherryBombs >= 3 && smokeDecoyBombs >= 3)
+                    {
+
+                        break;
+                    }
+
+                    continue;
+                }
+                else if (sum == 120)
+                {
+                    smokeDecoyBombs++;
+
+                    if (daturaBombs >= 3 && cherryBombs >= 3 && smokeDecoyBombs >= 3)
+                    {
+                        break;
+                    }
+
+                    continue;
+                }
+
+                else
+                {
+                    while (true)
+                    {
+                        effect -= 5;
+                        sum = effect + casing;
+
+                        if (sum == 40)
+                        {
+                            daturaBombs++;
+
+                            if (daturaBombs >= 3 && cherryBombs >= 3 && smokeDecoyBombs >= 3)
+                            {
+
+                                break;
+                            }
+
+                            break;
+                        }
+
+                        else if (sum == 60)
+                        {
+                            cherryBombs++;
+
+                            if (daturaBombs >= 3 && cherryBombs >= 3 && smokeDecoyBombs >= 3)
+                            {
+                                break;
+                            }
+
+                            break;
+                        }
+                        else if (sum == 120)
+                        {
+                            smokeDecoyBombs++;
+
+                            if (daturaBombs >= 3 && cherryBombs >= 3 && smokeDecoyBombs >= 3)
+                            {
+                                break;
+                            }
+
+                            break;
+                        }
+
+                    }
+                }
+
+
+
+            }
+
+            if (daturaBombs >= 3 && cherryBombs >= 3 && smokeDecoyBombs >= 3)
+            {
+                Console.WriteLine("Bene! You have successfully filled the bomb pouch!");
+            }
+            else
+            {
+                Console.WriteLine("You don't have enough materials to fill the bomb pouch.");
+            }
+
+            if (bombEffects.Count == 0)
+            {
+                Console.WriteLine("Bomb Effects: empty");
+            }
+
+            else
+            {
+
+                Console.WriteLine("Bomb Effects: " + string.Join(", ", bombEffects));
+            }
+
+
+            if (bombCasings.Count == 0)
+            {
+                Console.WriteLine("Bomb Casings: empty");
+            }
+
+            else
+            {
+                Console.WriteLine($"Bomb Casings: {string.Join(", ", bombCasings)}");
+            }
+
+            Console.WriteLine($"Cherry Bombs: {cherryBombs}");
+            Console.WriteLine($"Datura Bombs: {daturaBombs}");
+            Console.WriteLine($"Smoke Decoy Bombs: {smokeDecoyBombs}");
+
+        }
+
+
+    }
+
+}
---
+++ TheFightForGondor/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Channels;
+
+namespace TheFightForGondor
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int wavesNumber = int.Parse(Console.ReadLine());
+
+            Queue<int> plates = new Queue<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
+
+            int newPlate = 0;
+            int currentPlate = 0;
+            int currentOrc = 0;
+
+            int plate = 0;
+            int orc = 0;
+
+            Stack<int> orcs = null;
+
+
+            for (int i = 1; i <= wavesNumber; i++)
+            {
+                
+                orcs = new Stack<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
+
+               
+
+                 if (i % 3 == 0)
+                 {
+                     newPlate = int.Parse(Console.ReadLine());
+                    plates.Enqueue(newPlate);
+                 }
+                 
+
+                
+                 while (orcs.Count>0 && plates.Count>0)
+                 {
+                     plate = plates.Peek();
+                     orc = orcs.Peek();
+
+                     if (plate>orc)
+                    {
+                        currentPlate = plate - orc;
+                        plates.Dequeue();
+                        plates.Enqueue(currentPlate);
+                        orcs.Pop();
+                    }
+
+                    else if (plate==orc)
+                    {
+                        orcs.Pop();
+                        plates.Dequeue();
+                    }
+
+                    else if (orc>plate)
+                    {
+                        plates.Dequeue();
+                        currentOrc = orc - plate;
+                        orcs.Pop();
+                        orcs.Push(currentOrc);
+                    }
+
+                 }
+
+                 if (plates.Count==0)
+                 {
+                     
+                     break;
+                 }
+
+                 
+                
+            }
+
+            if (plates.Count!=0)
+            {
+                Console.WriteLine("The people successfully repulsed the orc's attack.");
+            }
+            else
+            {
+                Console.WriteLine("The orcs successfully destroyed the Gondor's defense.");
+            }
+
+            if (orcs.Count!=0)
+            {
+                Console.WriteLine($"Orcs left: {string.Join(", ", orcs)}");
+            }
+
+            if (plates.Count!= 0)
+            {
+                Console.WriteLine($"Plates left: {string.Join(", ", plates)}");
+            }
+
+
+        }
+    }
+}
